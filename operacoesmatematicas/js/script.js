362942492e6b1179124baa54e8baba01fcaf1b9c
WELLINGTON CARLOS DA SILVA LEITE <wellington.csleite@senacsp.edu.br>
2024-08-21 10:06:59 -0300

    prática variáveis,interpolação,opeações matemáticas

---
+++ operacoesmatematicas/js/script.js
@@ -0,0 +1,40 @@
+
+// soma
+
+console.log(5+3);
+
+
+// subtração
+
+console.log(3-2);
+
+const c = 8;
+const d = 3 ;
+const e = c - d;
+
+console.log(e);
+
+// multiplicação
+;
+const f = 2
+const g = f*2;
+
+console.log(g)
+
+console.log(4*2);
+
+// divisão
+console.log(8/4);
+
+
+// modulo
+
+console.log(5%2);
+
+const h = 10;
+const i = 5;
+
+console.log(h/i);
+
+console.log(h%i);
+
---
+++ variaveiseinterpolacao/js/script.js
@@ -0,0 +1,67 @@
+// comentário de uma linha
+
+/* comentários de variás linhas*/
+
+// exibindo um número na tela
+
+console.log(typeof 2.5);
+
+
+// declarando constante e imprimir
+
+const meuNumero = 10;
+
+console.log(meuNumero);
+
+// error ao alterar a constatnte
+
+const numero = 5;
+
+console.log(numero);
+
+// strings utilizando aspas simples aspas duplas
+
+console.log('estou aprendendo javaScript');
+console.log("estou aprendendo");
+console.log(`estou aprendendo`);
+
+const nome = `wellington`;
+
+console.log(nome.length);
+
+// pula uma linha
+
+console.log('vou pular uma linha \n R$50,00');
+
+// Espaçamento na string
+
+console.log('espaço \t na string');
+
+// varáveis var e let
+// var para escopo global
+// let para escopo local
+
+var numero1 = 5
+numero1 = 3
+
+console.log(numero1)
+
+let numero2 = 10;
+
+numero2 = 3 
+
+console.log(numero2)
+
+
+// interpolação
+
+const sobrenome = 'cunha'
+
+console.log(`olá, ${sobrenome}`);
+
+const idade = 34 
+
+console.log(`sua idade é ${idade} anos`)
+
+console.log(`seu sobre nome é ${sobrenome} e sua idade é ${idade}`)
+
